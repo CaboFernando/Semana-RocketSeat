c949c7bbfa2cd4c7a974a9417d6c76e8c6abf55f
Carlos <fer.1972@hotmail.com>
2019-10-01 16:50:32 -0300

    Backend Node.Js - 1:07:27

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://cbfernando:cbfernando@cluster0-niltr.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//GET, POST, PUT, DELETE
+
+// req.query => Acessar query params (para filtros)
+// req.params => Acessar route params (para edição, delete)
+// req.body => Acessar corpo da requisição (para criação, edição)
+
 app.use(express.json());
-
-app.get('/users', (req, res) => {
-    return res.json({ idade: req.query.idade});
-});
-
-app.post('/users', (req, res) => {
-    return res.json(req.body);
-});
-
-app.put('/users/:id', (req, res) => {
-    return res.json({ id: req.params.id});
-});
-
+app.use(routes);
 
 
 app.listen(3333);
